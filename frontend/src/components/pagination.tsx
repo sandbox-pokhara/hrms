1114bcb067615725b05d264746ac641adc7e8f33
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2024-07-22 01:59:24 +0545

    refactor: convert pagination to server component

--- frontend/src/components/pagination.tsx
+++ frontend/src/components/pagination.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
@@ -7,7 +5,7 @@ import {
   DoubleArrowRightIcon,
 } from "@radix-ui/react-icons";
 import { Button } from "@/components/ui/button";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import Link from "next/link";
 
 interface Props {
   totalPages: number;
@@ -15,21 +13,6 @@ interface Props {
 }
 
 export default function Pagination({ totalPages, pageIndex }: Props) {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const router = useRouter();
-
-  const createPageURL = (pageNumber: number | string) => {
-    const params = new URLSearchParams(searchParams);
-    params.set("page", pageNumber.toString());
-    return `${pathname}?${params.toString()}`;
-  };
-
-  const handleNavigation = (pageNumber: number) => {
-    const url = createPageURL(pageNumber);
-    router.push(url);
-  };
-
   return (
     <div className="flex flex-wrap items-center justify-between gap-2 mt-1 whitespace-nowrap sm:flex-nowrap">
       <span className="mt-4 text-sm text-muted-foreground">
@@ -38,42 +21,48 @@ export default function Pagination({ totalPages, pageIndex }: Props) {
       <div className="flex flex-wrap items-center gap-2 sm:gap-4 sm:flex-nowrap">
         <div className="flex items-center space-x-2">
           <Button
+            asChild
             variant="outline"
             className="hidden w-8 h-8 p-0 lg:flex"
-            onClick={() => handleNavigation(1)}
             disabled={pageIndex === 1}
           >
-            <span className="sr-only">Go to last page</span>
-            <DoubleArrowLeftIcon className="w-4 h-4" />
+            <Link href="/time-logs/?page=1">
+              <span className="sr-only">Go to last page</span>
+              <DoubleArrowLeftIcon className="w-4 h-4" />
+            </Link>
           </Button>
           <Button
+            asChild
             variant="outline"
             className="w-8 h-8 p-0"
-            onClick={() => handleNavigation(pageIndex - 1)}
             disabled={pageIndex <= 1}
           >
-            <span className="sr-only">Go to previous page</span>
-            <ChevronLeftIcon className="w-4 h-4" />
+            <Link href={`/time-logs/?page=${pageIndex - 1}`}>
+              <span className="sr-only">Go to previous page</span>
+              <ChevronLeftIcon className="w-4 h-4" />
+            </Link>
           </Button>
           <Button
+            asChild
             variant="outline"
             className="w-8 h-8 p-0"
-            onClick={() => {
-              handleNavigation(pageIndex + 1);
-            }}
             disabled={!(totalPages - pageIndex)}
           >
-            <span className="sr-only">Go to next page</span>
-            <ChevronRightIcon className="w-4 h-4" />
+            <Link href={`/time-logs/?page=${pageIndex + 1}`}>
+              <span className="sr-only">Go to next page</span>
+              <ChevronRightIcon className="w-4 h-4" />
+            </Link>
           </Button>
           <Button
+            asChild
             variant="outline"
             className="hidden w-8 h-8 p-0 lg:flex"
-            onClick={() => handleNavigation(totalPages)}
             disabled={!(totalPages - pageIndex)}
           >
-            <span className="sr-only">Go to last page</span>
-            <DoubleArrowRightIcon className="w-4 h-4" />
+            <Link href={`/time-logs/?page=${totalPages}`}>
+              <span className="sr-only">Go to last page</span>
+              <DoubleArrowRightIcon className="w-4 h-4" />
+            </Link>
           </Button>
         </div>
       </div>
