4fa121a76306dde1d0dabf5241f680489a76c88a
callback cat <76839908+chamanbravo@users.noreply.github.com>
2024-07-22 19:14:09 +0545

    feat: redirect to dashboard if logged in

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -7,6 +7,8 @@ import {
 } from "@/components/ui/card";
 import type { Metadata } from "next";
 import LoginForm from "@/components/login-form";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Login - Sandbox HRMS",
@@ -14,6 +16,10 @@ export const metadata: Metadata = {
 };
 
 export default function Login() {
+  const cookieStore = cookies();
+  const sessionid = cookieStore.get("sessionid");
+  if (sessionid) redirect("/");
+
   return (
     <div className="h-screen flex justify-center items-center">
       <Card>
