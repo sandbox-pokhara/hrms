d7d3176d7e7bef3341cc71d420c017b4347d006c
callback cat <76839908+chamanbravo@users.noreply.github.com>
2024-07-23 22:10:36 +0545

    feat: time summary page

--- frontend/src/app/time-summary/page.tsx
+++ frontend/src/app/time-summary/page.tsx
@@ -1,14 +1,36 @@
 import MainLayout from "@/components/main-layout";
-import PageUnderConstruction from "@/components/page-under-construction";
-import { getCurrentUser } from "@/lib/apiServer";
 import type { Metadata } from "next";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import TimeLogStart from "@/components/time-log-start";
+import { getCurrentUser, getTimeSummary } from "@/lib/apiServer";
+import { DatePickerWithRange } from "@/components/date-range-picker";
+import { format, subDays } from "date-fns";
 
 export const metadata: Metadata = {
   title: "Time Summary - Sandbox HRMS",
   description: "Human Resource Management System",
 };
 
-export default async function TimeLogs() {
+export default async function TimeSummary({
+  searchParams,
+}: {
+  searchParams?: {
+    start?: string;
+    end?: string;
+  };
+}) {
+  const start =
+    searchParams?.start || format(subDays(new Date(), 6), "yyyy-MM-dd");
+  const end = searchParams?.end || format(new Date(), "yyyy-MM-dd");
+
+  const timeSummary = await getTimeSummary(start, end);
   const currentUser = await getCurrentUser();
 
   return (
@@ -16,7 +38,47 @@ export default async function TimeLogs() {
       <div className="flex items-center">
         <h1 className="text-lg font-semibold md:text-2xl">Time Summary</h1>
       </div>
-      <PageUnderConstruction />
+      <div className="ml-auto">
+        <DatePickerWithRange initialFrom={start} initialTo={end} />
+      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>User</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead>Expected Hour</TableHead>
+            <TableHead>Hours Worked</TableHead>
+            <TableHead>Weekday</TableHead>
+            <TableHead>Holiday</TableHead>
+          </TableRow>
+        </TableHeader>
+        {timeSummary?.length ? (
+          <TableBody>
+            {timeSummary?.map((userSummary) =>
+              userSummary?.summary.map((item, i) => (
+                <TableRow key={i}>
+                  <TableCell>{userSummary.user}</TableCell>
+                  <TableCell>
+                    <TimeLogStart start={item.date} />
+                  </TableCell>
+                  <TableCell>{item.expected_hours}</TableCell>
+                  <TableCell>{item.hours_worked.toFixed(2)}</TableCell>
+                  <TableCell>{item.weekday}</TableCell>
+                  <TableCell>{item.holiday || "-"}</TableCell>
+                </TableRow>
+              ))
+            )}
+          </TableBody>
+        ) : (
+          <TableBody>
+            <TableRow>
+              <TableCell colSpan={7} className="text-center">
+                No time summary data found.
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        )}
+      </Table>
     </MainLayout>
   );
 }
