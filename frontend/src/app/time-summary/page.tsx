148f4eb7f1fea822045bc4f2ff310d67deaa8d50
callback cat <76839908+chamanbravo@users.noreply.github.com>
2024-07-24 01:04:45 +0545

    feat: time summary page

--- frontend/src/app/time-summary/page.tsx
+++ frontend/src/app/time-summary/page.tsx
@@ -8,15 +8,21 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import TimeLogStart from "@/components/time-log-start";
 import { getCurrentUser, getTimeSummary } from "@/lib/apiServer";
-import { DatePickerWithRange } from "@/components/date-range-picker";
-import { format, subDays } from "date-fns";
+import { format, startOfWeek, endOfWeek, eachDayOfInterval } from "date-fns";
+import { cn, convertHoursToHHMM } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Time Summary - Sandbox HRMS",
   description: "Human Resource Management System",
 };
+
+interface IWorkingHours {
+  total: number;
+  [date: string]: number;
+}
 
 export default async function TimeSummary({
   searchParams,
@@ -24,60 +30,176 @@ export default async function TimeSummary({
   searchParams?: {
     start?: string;
     end?: string;
+    toggle?: boolean;
   };
 }) {
-  const start =
-    searchParams?.start || format(subDays(new Date(), 6), "yyyy-MM-dd");
-  const end = searchParams?.end || format(new Date(), "yyyy-MM-dd");
+  const today = new Date();
+  const start = searchParams?.start
+    ? new Date(searchParams.start)
+    : startOfWeek(today, { weekStartsOn: 0 });
+  const end = searchParams?.end
+    ? new Date(searchParams.end)
+    : endOfWeek(today, { weekStartsOn: 0 });
+  const toggle = searchParams?.toggle || false;
 
-  const timeSummary = await getTimeSummary(start, end);
+  const formattedStart = format(start, "yyyy-MM-dd");
+  const formattedEnd = format(end, "yyyy-MM-dd");
+
+  const timeSummary = await getTimeSummary(formattedStart, formattedEnd);
   const currentUser = await getCurrentUser();
+  const dateRange = eachDayOfInterval({ start, end });
+
+  const totalWorkingHours: IWorkingHours | undefined =
+    timeSummary?.reduce<IWorkingHours>(
+      (acc, user) => {
+        user.summary.forEach((day) => {
+          if (!acc[day?.date]) {
+            acc[day?.date] = 0;
+          }
+          acc[day?.date] += day.hours_worked;
+          acc.total += day.hours_worked;
+        });
+        return acc;
+      },
+      { total: 0 }
+    );
 
   return (
     <MainLayout currentUser={currentUser} active="time-summary">
-      <div className="flex items-center">
+      <div className="flex items-center justify-between mb-4">
         <h1 className="text-lg font-semibold md:text-2xl">Time Summary</h1>
-      </div>
-      <div className="ml-auto">
-        <DatePickerWithRange initialFrom={start} initialTo={end} />
+        <Button variant="outline" asChild>
+          <Link
+            href={
+              toggle
+                ? "/time-summary/?toggle=false"
+                : "/time-summary/?toggle=true"
+            }
+          >
+            Show Difference
+          </Link>
+        </Button>
       </div>
       <Table>
         <TableHeader>
           <TableRow>
             <TableHead>User</TableHead>
-            <TableHead>Date</TableHead>
-            <TableHead>Expected Hour</TableHead>
-            <TableHead>Hours Worked</TableHead>
-            <TableHead>Weekday</TableHead>
-            <TableHead>Holiday</TableHead>
+            <TableHead>Total</TableHead>
+            {dateRange.map((date) => (
+              <TableHead key={date.toISOString()}>
+                {format(date, "EEE dd")}
+              </TableHead>
+            ))}
           </TableRow>
         </TableHeader>
-        {timeSummary?.length ? (
-          <TableBody>
-            {timeSummary?.map((userSummary) =>
-              userSummary?.summary.map((item, i) => (
-                <TableRow key={i}>
-                  <TableCell>{userSummary.user}</TableCell>
-                  <TableCell>
-                    <TimeLogStart start={item.date} />
-                  </TableCell>
-                  <TableCell>{item.expected_hours}</TableCell>
-                  <TableCell>{item.hours_worked.toFixed(2)}</TableCell>
-                  <TableCell>{item.weekday}</TableCell>
-                  <TableCell>{item.holiday || "-"}</TableCell>
-                </TableRow>
-              ))
-            )}
-          </TableBody>
-        ) : (
-          <TableBody>
+        <TableBody>
+          {timeSummary?.length ? (
+            <>
+              {timeSummary.map((userSummary) => {
+                const userTotalHoursWorked = userSummary.summary.reduce(
+                  (acc, item) => item.hours_worked + acc,
+                  0
+                );
+                const userExpectedHours = userSummary.summary.reduce(
+                  (acc, item) => {
+                    if (new Date(item.date) > new Date() || item.holiday.length)
+                      return acc;
+                    return item.expected_hours + acc;
+                  },
+                  0
+                );
+
+                return (
+                  <TableRow key={userSummary.user}>
+                    <TableCell>{userSummary.user}</TableCell>
+                    <TableCell
+                      className={cn(
+                        "font-medium",
+                        toggle
+                          ? userTotalHoursWorked - userExpectedHours >= 0
+                            ? "text-green-600"
+                            : "text-red-600"
+                          : "text-blue-600"
+                      )}
+                    >
+                      {toggle &&
+                        userTotalHoursWorked - userExpectedHours >= 0 &&
+                        "+"}
+                      {convertHoursToHHMM(
+                        toggle
+                          ? userTotalHoursWorked - userExpectedHours
+                          : userTotalHoursWorked
+                      )}
+                    </TableCell>
+                    {dateRange.map((date) => {
+                      const dayData = userSummary.summary.find(
+                        (item) => item.date === format(date, "yyyy-MM-dd")
+                      );
+                      return (
+                        <TableCell
+                          key={date.toISOString()}
+                          className={cn({
+                            "bg-muted":
+                              dayData?.weekday === "sat" ||
+                              dayData?.holiday.length,
+                            "text-green-600":
+                              toggle &&
+                              (dayData?.hours_worked || 0) -
+                                (dayData?.expected_hours || 0) >=
+                                0,
+                            "text-red-600":
+                              toggle &&
+                              (dayData?.hours_worked || 0) -
+                                (dayData?.expected_hours || 0) <
+                                0,
+                          })}
+                        >
+                          {toggle &&
+                            !dayData?.weekday &&
+                            (dayData?.hours_worked || 0) -
+                              (dayData?.expected_hours || 0) >=
+                              0 &&
+                            "+"}
+                          {dayData?.holiday
+                            ? ""
+                            : toggle
+                            ? convertHoursToHHMM(
+                                (dayData?.hours_worked || 0) -
+                                  (dayData?.expected_hours || 0)
+                              )
+                            : dayData?.hours_worked
+                            ? convertHoursToHHMM(dayData?.hours_worked)
+                            : ""}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+              <TableRow className="bg-muted font-medium">
+                <TableCell>Total Working Hours</TableCell>
+                <TableCell>
+                  {convertHoursToHHMM(totalWorkingHours?.total || 0)}
+                </TableCell>
+                {dateRange.map((date) => {
+                  const totalHours =
+                    totalWorkingHours?.[format(date, "yyyy-MM-dd")] || 0;
+                  return (
+                    <TableCell key={date.toISOString()}>
+                      {convertHoursToHHMM(totalHours)}
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            </>
+          ) : (
             <TableRow>
-              <TableCell colSpan={7} className="text-center">
+              <TableCell colSpan={9} className="text-center">
                 No time summary data found.
               </TableCell>
             </TableRow>
-          </TableBody>
-        )}
+          )}
+        </TableBody>
       </Table>
     </MainLayout>
   );
